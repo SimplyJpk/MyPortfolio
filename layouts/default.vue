2d7c0f492c9a932eccec29c0f5018b26eca5e19b
James Kellie <Jpk_GameID@hotmail.com>
2021-04-26 15:41:18 +1000

    Replace simplyjpk with name

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@ export default {
   mixins: [VueScreenSize.VueScreenSizeMixin],
   head () {
     return {
-      title: 'SimplyJpk\'s Website!'
+      title: 'James Kellie\'s Website!'
     }
   }
 }
