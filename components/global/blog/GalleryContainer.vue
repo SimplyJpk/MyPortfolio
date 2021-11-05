ac889fd405ee1095caa42c6fbeb034aae21ea906
James Kellie <Jpk_GameID@hotmail.com>
2021-11-05 21:54:08 +1100

    Blog Format Gallery
    Shows images in a formattable way, still a bit rough around the edges.
    
    BlogImage also can show images in the global modal.

---
+++ components/global/blog/GalleryContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="gallery-container">
+    <h2 v-if="title !== ''">
+      {{ title }}
+    </h2>
+    <b-container :style="containerStyle" class="gallery-container__gallery">
+      <slot />
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GalleryContainer',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    width: {
+      type: String,
+      required: false,
+      default: '100%'
+    }
+  },
+  computed: {
+    containerStyle () {
+      return {
+        width: this.width
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.gallery-container {
+  display: block;
+}
+.gallery-container__gallery {
+}
+</style>
